cd56f6e0c0ef0df9cfeff90478b1b6607a5b901d
Penalver-Ezequiel <ezepenalver29@gmail.com>
2024-11-13 11:23:58 -0300

    Creacion de proyecto Spring

---
+++ src/main/java/com/irso/proyecto_crud/ProyectoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.irso.proyecto_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irso/proyecto_crud/ProyectoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irso.proyecto_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
